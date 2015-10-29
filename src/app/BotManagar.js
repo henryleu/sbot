ac3179be741f96a47d3e574b6f81a831024e3171
leekangtaqi <leekangtaqi@sina.com>
2015-10-29 15:58:57 +0800

    do some refacotr

--- src/app/BotManagar.js
+++ src/app/BotManagar.js
@@ -1,18 +1,21 @@
-var Bots = (function(){
-    var botsContainer = {};
-    function Bots(){}
-    Bots.prototype.getBotById = function(id){
-        return botsContainer[id];
-    };
-    Bots.prototype.setBot = function(bot){
-        botsContainer[bot.id] = bot;
-    };
-    Bots.prototype.removeBot = function(id){
-        delete botsContainer[id];
-    };
-    Bots.prototype.getBots = function(){
-        return botsContainer;
-    };
-    return Bots;
-}());
-module.exports = new Bots();
+var botsContainer = {};
+
+function BotManagar(){}
+
+BotManagar.prototype.getBotById = function(id){
+    return botsContainer[id];
+};
+
+BotManagar.prototype.setBot = function(bot){
+    botsContainer[bot.id] = bot;
+};
+
+BotManagar.prototype.removeBot = function(id){
+    delete botsContainer[id];
+};
+
+BotManagar.prototype.getBots = function(){
+    return botsContainer;
+};
+
+module.exports = new BotManagar();
