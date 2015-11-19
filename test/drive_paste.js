803158f4004fe0f772d68dc5330f1adcf05dcdd8
leekangtaqi <leekangtaqi@sina.com>
2015-11-19 19:45:40 +0800

    add some test

--- test/drive_paste.js
+++ test/drive_paste.js
@@ -9,22 +9,40 @@ var driver = new webdriver.Builder().withCapabilities(browser).build();
 var websiteUrl = 'https://www.baidu.com/';
 driver.get(websiteUrl);
 console.log('requesting ' + websiteUrl + ' with ' + browserName);
-
+driver.call(function(){
+    console.log('loading complete...')
+});
 var itemEl = driver.findElement(webdriver.By.id('kw'));
 itemEl.sendKeys('ta');
+driver.call(function(){
+    console.log('type text ta...')
+});
 driver.sleep(1000);
 itemEl.sendKeys(webdriver.Key.chord(webdriver.Key.CONTROL, 'a'));
+driver.call(function(){
+    console.log('type ctrl a...')
+});
 driver.sleep(1000);
 itemEl.sendKeys(webdriver.Key.chord(webdriver.Key.CONTROL, 'c'));
+driver.call(function(){
+    console.log('type ctrl c...')
+});
 driver.sleep(1000);
 itemEl.sendKeys(webdriver.Key.chord(webdriver.Key.CONTROL, 'v'));
+driver.call(function(){
+    console.log('type ctrl v...')
+});
 driver.sleep(1000);
 itemEl.sendKeys(webdriver.Key.chord(webdriver.Key.CONTROL, 'v'));
+driver.call(function(){
+    console.log('type ctrl v...')
+});
 
 driver.findElement(webdriver.By.id('su')).click();
 
 driver.wait(function () {
     return driver.getTitle().then(function (title) {
+        console.log(title);
         var ret = title === 'tata_百度搜索';
         ret && console.log('drive successfully!');
         return ret;
