3c79983acdc6f56123cd18d393716d5421973087
leekangtaqi <leekangtaqi@sina.com>
2015-11-19 16:37:36 +0800

    rename

--- src/util/pasteFromClipboard.py
+++ src/util/pasteFromClipboard.py
@@ -7,8 +7,8 @@ import sys
 import time
 
 def paste_image():
-clipboard = gtk.clipboard_get()
-clipboard.request_image(handler, '123')
+    clipboard = gtk.clipboard_get()
+    clipboard.request_image(handler, '123')
 
 def handler(arg1, buf, arg3):
 print str(buf.get_width())
