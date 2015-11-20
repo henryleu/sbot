fd168ce3d5e4d3d7db6377931f0f74155ad0bf6a
leekangtaqi <leekangtaqi@sina.com>
2015-11-20 11:12:55 +0800

    test

--- test/copy_paste_text.py
+++ test/copy_paste_text.py
@@ -26,29 +26,3 @@ def handler(arg1, arg2, arg3):
 copy_image();
 time.sleep(1);
 paste_image();
-
-
-#! /usr/bin/python
-import pygtk
-pygtk.require('2.0')
-import gtk
-import os
-import sys
-import time
-
-def copy_image():
-    clipboard = gtk.clipboard_get(gtk.gdk.SELECTION_CLIPBOARD)
-    clipboard.set_text("hello world")
-    clipboard.store()
-
-def paste_image():
-    clipboard = gtk.clipboard_get(gtk.gdk.SELECTION_CLIPBOARD)
-    clipboard.request_text(handler, -1)
-
-def handler(arg1, buf, arg3):
-    print "**************"
-    print buf;
-
-copy_image();
-time.sleep(1);
-paste_image();
