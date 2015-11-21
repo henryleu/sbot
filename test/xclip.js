5490666f0a40eb8528b81464b6e4c0caf2460748
leekangtaqi <leekangtaqi@sina.com>
2015-11-21 12:55:49 +0800

    ok

--- test/xclip.js
+++ test/xclip.js
@@ -1,7 +1,14 @@
-var copy_paste = require('copy-paste');
+//var copy_paste = require('copy-paste');
+//
+//var stream = require('fs').createReadStream('./clipboard.jpg');
+//
+//copy_paste.copy(stream, function(){
+//    console.log('ok')
+//})
+var exec = require('child_process').exec;
 
-var stream = require('fs').createReadStream('./clipboard.jpg');
-
-copy_paste.copy(stream, function(){
-    console.log('ok')
-})
+exec('xclip -selection clipboard clipboard.jpg',
+    function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+    })
